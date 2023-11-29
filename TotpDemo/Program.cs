d0bf4883c43bbd6cbb82276cecb306dd20fc0e7f
Lê Văn Minh <minhlv@meeyland.com>
2023-11-29 13:57:37 +0700

    demo google authen

--- TotpDemo/Program.cs
+++ TotpDemo/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace TotpDemo
+﻿using Google.Authenticator;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace TotpDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string key = "123abc";
+
+            TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+            SetupCode setupInfo = tfa.GenerateSetupCode("Test Two Factor", "user@example.com", key, false, 3);
+
+            string qrCodeImageUrl = setupInfo.QrCodeSetupImageUrl;
+            string manualEntrySetupCode = setupInfo.ManualEntryKey;
+
+            //imgQrCode.ImageUrl = qrCodeImageUrl;
+            //lblManualSetupCode.Text = manualEntrySetupCode;
+
+            // verify
+            TwoFactorAuthenticator tfa2 = new TwoFactorAuthenticator();
+            Console.Write("Nhap otp: ");
+            bool result = tfa.ValidateTwoFactorPIN(key, Console.ReadLine());
+            Console.Write(result);
         }
     }
 }
