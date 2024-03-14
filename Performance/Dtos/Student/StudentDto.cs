a7027fd80d6f14e1ef30aa40bd0ccf4268df1038
Lê Văn Minh <vanminh.dev@gmail.com>
2024-03-14 11:14:24 +0700

    update

--- Performance/Dtos/Student/StudentDto.cs
+++ Performance/Dtos/Student/StudentDto.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +12,15 @@ namespace Performance.Dtos.Student
     internal class StudentDto
     {
         public int Id { get; set; }
+
+        [AllowedValues()]
         public string Name { get; set; } = null!;
     }
+
+    public static class StudentConst
+    {
+        public const string Name1 = "Name1";
+    }
+
+
 }
