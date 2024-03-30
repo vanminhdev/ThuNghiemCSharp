469a96f28b25e279010e18083eaf545efb6e2da5
Lê Văn Minh <vanminh.dev@gmail.com>
2024-03-30 21:26:00 +0700

    demo cache

---
+++ WebAPICacheDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+
+namespace WebAPICacheDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddResponseCaching();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddResponseCaching(options =>
+            {
+                options.MaximumBodySize = 1024;
+                options.UseCaseSensitivePaths = true; //đường dẫn phân biệt chữ hoa chữ thường
+                options.SizeLimit = 1024 * 1024 * 1024; //1GB
+            });
+
+            builder.Services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "image/jpeg" }
+                );
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseResponseCaching();
+            //app.UseResponseCompression();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
